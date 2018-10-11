fe3e3750a2c6fb068cb7c60152b9874a563a8709
curben <curben@users.noreply.gitlab.com>
2018-10-11 11:41:34 +1030

    Use gunzip instead of zcat
    alpine's zcat supports only gzip/bzip2/xz not zip

--- utils/umbrella-top-1m.sh
+++ utils/umbrella-top-1m.sh
@@ -8,7 +8,7 @@
 wget https://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip -O top-1m.csv.zip
 
 # Decompress the zip and write output to stdout
-zcat -dc top-1m.csv.zip | \
+gunzip -dc top-1m.csv.zip | \
 # Parse domains only
 cut -f 2 -d ',' > ../src/top-1m.txt
 
