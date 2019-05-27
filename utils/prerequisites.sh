9a5fdb2be621b82afae5c88407d30e72fe58c0b5
curben <curben@users.noreply.gitlab.com>
2019-05-27 15:59:08 +0930

    fix: use simple URL list
    we no longer care the status of URL
    bb817d9838d35eda22c5b30018488be0bc19e0db

--- utils/prerequisites.sh
+++ utils/prerequisites.sh
@@ -3,10 +3,24 @@
 set -e -x
 
 # Download URLhaus database
-wget https://urlhaus.abuse.ch/downloads/csv/ -O ../src/URLhaus.csv
+wget https://urlhaus.abuse.ch/downloads/text/ -O ../src/URLhaus.txt
 
 # Download Cisco Umbrella 1 Million
 wget https://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip -O top-1m.csv.zip
 
-cp ../src/URLhaus.csv .
 cp ../src/exclude.txt .
+
+## Clean up URLhaus.txt
+cat ../src/URLhaus.txt | \
+# Remove comment
+sed '/^#/ d' | \
+# Convert DOS to Unix line ending
+dos2unix | \
+# Remove http(s)://
+cut -f 3 -d '/' | \
+# Remove www
+# Only matches domains that start with www
+# Not examplewww.com
+sed 's/^www\.//g' | \
+# Sort and remove duplicates
+sort -u > URLhaus.txt
