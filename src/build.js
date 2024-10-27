a4374b02e0c966bfb505cd8cccee59f892d0d964
MDLeom <2809763-curben@users.noreply.gitlab.com>
2024-10-27 06:04:51 +0000

    refactor: extract zip from stream

--- src/build.js
+++ src/build.js
@@ -2,19 +2,16 @@
 
 // for deployment outside of GitLab CI, e.g. Cloudflare Pages and Netlify
 
-import unzip from 'extract-zip'
+import { Extract } from 'unzipper'
 import { dirname, join } from 'node:path'
 import { mkdir } from 'node:fs/promises'
-import { createWriteStream } from 'node:fs'
 import { pipeline } from 'node:stream/promises'
 import { fileURLToPath } from 'node:url'
 import { Readable } from 'node:stream'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const rootPath = join(__dirname, '..')
-const tmpPath = join(rootPath, 'tmp')
 const publicPath = join(rootPath, 'public')
-const zipPath = join(tmpPath, 'artifacts.zip')
 const artifactsUrl = 'https://gitlab.com/malware-filter/urlhaus-filter/-/jobs/artifacts/main/download?job=pages'
 const pipelineUrl = 'https://gitlab.com/malware-filter/urlhaus-filter/badges/main/pipeline.svg'
 const ghMirror = 'https://nightly.link/curbengh/urlhaus-filter/workflows/pages/main/public.zip'
@@ -30,15 +27,11 @@ const pipelineStatus = async (url) => {
 }
 
 const f = async () => {
-  let isMirror = false
-
-  await mkdir(tmpPath, { recursive: true })
-
   console.log(`Downloading artifacts.zip from "${artifactsUrl}"`)
   try {
     await pipeline(
       Readable.fromWeb((await fetch(artifactsUrl)).body),
-      createWriteStream(zipPath)
+      Extract({ path: rootPath })
     )
     await pipelineStatus(pipelineUrl)
   } catch ({ message }) {
@@ -48,12 +41,13 @@ const f = async () => {
     }))
 
     console.log(`Downloading artifacts.zip from "${ghMirror}"`)
-    isMirror = true
+
+    await mkdir(publicPath, { recursive: true })
 
     try {
       await pipeline(
         Readable.fromWeb((await fetch(ghMirror)).body),
-        createWriteStream(zipPath)
+        Extract({ path: publicPath })
       )
     } catch ({ message }) {
       throw new Error(JSON.stringify({
@@ -61,14 +55,6 @@ const f = async () => {
         link: ghMirror
       }))
     }
-  }
-
-  console.log('Extracting artifacts.zip...')
-  if (isMirror === false) {
-    await unzip(zipPath, { dir: rootPath })
-  } else {
-    await mkdir(publicPath, { recursive: true })
-    await unzip(zipPath, { dir: publicPath })
   }
 }
 
